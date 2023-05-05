252e11be22538fb03400642c1c683aec38b7bc7d
harshblip <harshjipkate@gmail.com>
2023-05-05 15:14:44 +0530

    spinner while data is being fetched

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+const Spinner = () => (
+  <Fragment>
+    <div className="spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </Fragment>
+);
+
+export default Spinner;

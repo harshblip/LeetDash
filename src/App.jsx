d4249510f5417c73b0613e7401f9847246490628
harshblip <harshjipkate@gmail.com>
2023-05-06 01:26:32 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Spinner from "./components/layout/Spinner";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import User from './components/users/User.jsx';
+import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
-function App() {
-  const [count, setCount] = useState(0);
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
+import './App.css';
+
+const App = () => {
   return (
-    <>
-      <div>
-        <Spinner />
-      </div>
-    </>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
-}
+};
 
 export default App;

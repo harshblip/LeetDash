529b4bcbd0e632a10d7a510bc688b55e03f7665d
harshblip <harshjipkate@gmail.com>
2023-05-05 11:56:10 +0530

    creating alertContext for user alerts

---
+++ src/components/layout/Alert.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+// import AlertContext from ''
+
+const Alert = () => {
+    const alertContext = useContext(AlertContext);
+
+    const { alert } = alertContext;
+
+    return (
+        alert != null && (
+            <div className={`alert alert-${alert.type}`}>
+                <i className = 'fas fa-info-circle'/> {alert.msg}
+
+            </div>
+        )
+    )
+}
+
+export default Alert;
